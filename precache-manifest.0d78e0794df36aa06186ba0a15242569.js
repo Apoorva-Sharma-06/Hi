611fc42a11eab53b81056b1759bd4e0cc356e9dc
Apoorva-Sharma-06 <sharma.apoo@northeastern.edu>
2022-01-16 03:36:46 -0500

    Updates

---
+++ precache-manifest.0d78e0794df36aa06186ba0a15242569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c060f839072e801ac2",
+    "url": "/Hi/static/css/main.10134c7b.chunk.css"
+  },
+  {
+    "revision": "f8c060f839072e801ac2",
+    "url": "/Hi/static/js/main.f8c060f8.chunk.js"
+  },
+  {
+    "revision": "d1dbfb586bf6780bfeb4",
+    "url": "/Hi/static/js/runtime~main.d1dbfb58.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Hi/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9b4a30de376896ca1d4a47089effbb14",
+    "url": "/Hi/index.html"
+  }
+];
